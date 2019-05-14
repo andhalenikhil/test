af363ab4085e41708c69eef7b3ff4b1fd5504151
unknown <10925@INPUN0003DC.IZD01.in>
2019-05-14 15:09:56 +0530

    Added Hungry promp

--- Hungry.java
+++ Hungry.java
@@ -1,8 +1,18 @@
+import java.util.Scanner;
 
 public class Hungry {
 
 	public static void main(String args[]) {
 		
 		System.out.println("Hey there Im hungry");
+	
+	Scanner sc= new Scanner(System.in);
+	System.out.println("are you hungry yes/no");
+	if(sc.nextLine().equalsIgnoreCase("yes")) {
+		System.out.println("Im hungry");
+	}
+	else {
+		System.out.println("Do your homework");
+	}
 	}
 }
